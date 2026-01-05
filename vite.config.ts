b44d19ecfdb127092da08cb028c6d963035205bb
yassineAdraoui <62291655+yassineAdraoui@users.noreply.github.com>
2026-01-05 23:51:58 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
+        base: '/ems3_tools/',
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
